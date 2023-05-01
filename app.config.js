fae97152cd33869ff4bf24cf1fd6a0ec17a9956f
Jake Demian <jakedemian@gmail.com>
2023-04-30 20:46:42 -0400

    bundle id update

--- app.config.js
+++ app.config.js
@@ -1,35 +1,35 @@
 /* eslint-disable no-undef */
-import "dotenv/config";
+import 'dotenv/config';
 
 export default {
   expo: {
-    name: "nutri-vim",
-    slug: "nutri-vim",
-    version: "1.0.0",
-    orientation: "portrait",
-    icon: "./assets/icon.png",
-    userInterfaceStyle: "light",
+    name: 'nutri-vim',
+    slug: 'nutri-vim',
+    version: '1.0.0',
+    orientation: 'portrait',
+    icon: './assets/icon.png',
+    userInterfaceStyle: 'light',
     splash: {
-      image: "./assets/splash.png",
-      resizeMode: "contain",
-      backgroundColor: "#ffffff",
+      image: './assets/splash.png',
+      resizeMode: 'contain',
+      backgroundColor: '#ffffff',
     },
     updates: {
       fallbackToCacheTimeout: 0,
     },
-    assetBundlePatterns: ["**/*"],
+    assetBundlePatterns: ['**/*'],
     ios: {
-      bundleIdentifier: "nutri-vim",
+      bundleIdentifier: 'com.jakedemian.nutri-vim',
       supportsTablet: true,
     },
     android: {
       adaptiveIcon: {
-        foregroundImage: "./assets/adaptive-icon.png",
-        backgroundColor: "#FFFFFF",
+        foregroundImage: './assets/adaptive-icon.png',
+        backgroundColor: '#FFFFFF',
       },
     },
     web: {
-      favicon: "./assets/favicon.png",
+      favicon: './assets/favicon.png',
     },
     extra: {
       apiKey: process.env.FIREBASE_API_KEY,
